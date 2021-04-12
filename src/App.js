091fe137a8cccbbba608c7f0b5a529b30ecabd54
harsha <manyamharsha112@gmail.com>
2021-04-12 14:22:24 +0530

    firebase todo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import Todos from "./Todos";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "./firebase";
-import firebase from "firebase";
+// import { useAuthState } from "react-firebase-hooks/auth";
+// import { auth } from "./firebase";
+// import firebase from "firebase";
 
 const App = () => {
 
